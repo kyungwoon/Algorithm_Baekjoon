0284b2622d885e35baac92550c93e52554299c2a
kyungwoon <94433709+kyungwoon@users.noreply.github.com>
2021-11-17 12:41:01 +0900

    Update Main.java

--- src/bjo/입출력/10951번(A+B -4)/Main.java
+++ src/bjo/입출력/10951번(A+B -4)/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
         while(sc.hasNextInt()) {
         	int num1 = sc.nextInt();
-            int num2 = sc.nextInt();
+         int num2 = sc.nextInt();
             
             System.out.println(num1 + num2);
         }
