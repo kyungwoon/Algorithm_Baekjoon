e5bfcaf4d2cd2068852f973fdae3ce9e31c1d23c
Kyungwoon Na <nakw0410@gmail.com>
2021-11-17 13:23:21 +0900

    test

--- src/bjo_10951.java
+++ src/bjo_10951.java
@@ -1,8 +1,17 @@
+import java.util.Scanner;
 
 public class bjo_10951 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+        
+
+        while(sc.hasNextInt()) {
+        	int num1 = sc.nextInt();
+            int num2 = sc.nextInt();
+            
+            System.out.println(num1 + num2);
+        }
 
 	}
 
