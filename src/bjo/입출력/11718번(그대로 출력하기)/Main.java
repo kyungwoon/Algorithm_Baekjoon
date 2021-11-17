f6952cc89780d13ef8b3a85c15c966b45506aaa1
Kyungwoon Na <nakw0410@gmail.com>
2021-11-17 15:37:51 +0900

    test

--- src/bjo/입출력/11718번(그대로 출력하기)/Main.java
+++ src/bjo/입출력/11718번(그대로 출력하기)/Main.java
@@ -1,31 +1,18 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
 
-public class Main{
-	public static void main(String args[]) throws NumberFormatException, IOException {
-	
+public class Main {
+	public static void main(String[] args) throws IOException{
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
-		int T = Integer.parseInt(br.readLine());
-		StringTokenizer st;
+		String str ;
 		
-		for( int i = 1; i <=T; i++) {
-			st = new StringTokenizer(br.readLine());
-			bw.write("Case #" + i + ": ");
-			bw.write(Integer.parseInt(st.nextToken()) + 
-					Integer.parseInt(st.nextToken()) + "\n");
+		while((str = br.readLine()) != null) {
+			System.out.println(str);
 		}
 		
-		br.close();
-		bw.flush();
-		bw.close();
-		//test
 		
-			
+		}
 	}
-}
