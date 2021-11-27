2f34bad146814c5854fd21234979126ae62b6b0d
Kyungwoon Na <nakw0410@gmail.com>
2021-11-27 15:09:34 +0900

    feat: 백준 2742번 (기찍 N)

--- src_입출력/bjo_2742/Main.java
+++ src_입출력/bjo_2742/Main.java
@@ -7,14 +7,13 @@ import java.io.InputStreamReader;
 public class Main {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
-		
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int input = Integer.parseInt(br.readLine());
-		
-		for(int i = input; i > 0; i--) {
+
+		for (int i = input; i > 0; i--) {
 			System.out.println(i);
 		}
-		
 
 	}
 
