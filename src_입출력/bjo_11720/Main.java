44e4705c7bda2dd4923e8e313c96679a290197c2
Kyungwoon Na <nakw0410@gmail.com>
2021-11-27 14:09:22 +0900

    initial commit

---
+++ src_입출력/bjo_11720/Main.java
@@ -0,0 +1,23 @@
+package bjo_11720;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		String strInput = br.readLine();
+
+		int sum = 0;
+
+		for (int i = 0; i < T; i++) {
+			sum += strInput.charAt(i) - 48;
+		}
+		System.out.println(sum);
+	}
+
+}
