031d4905c55cb14140a661d51c75958e1133b190
Kyungwoon Na <nakw0410@gmail.com>
2021-11-27 14:25:23 +0900

    feat: 백준 11721번 (열 개씩 끊어 출력하기)

---
+++ src_입출력/bjo_11721/Main.java
@@ -0,0 +1,23 @@
+package bjo_11721;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String strInput = br.readLine();
+
+		for (int i = 0; i < strInput.length(); i++) {
+			System.out.print(strInput.charAt(i));
+			// 인덱스 i가 10의 배수인 경우 줄바꿈
+			if (i % 10 == 9) {
+				System.out.println();
+			}
+		}
+	}
+
+}
