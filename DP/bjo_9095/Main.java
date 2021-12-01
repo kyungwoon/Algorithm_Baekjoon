823aa6521286d2608ca8db225d8792dcd5c16eed
Kyungwoon Na <nakw0410@gmail.com>
2021-12-01 16:17:04 +0900

    feat: 백준 9095번 (1, 2, 3 더하기)

---
+++ DP/bjo_9095/Main.java
@@ -0,0 +1,29 @@
+package bjo_9095;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+
+		int[] dp = new int[11];
+		dp[1] = 1;
+		dp[2] = 2;
+		dp[3] = 4;
+
+		for (int i = 4; i < 11; i++) {
+			dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+		}
+
+		for (int i = 0; i < T; i++) {
+			int input = Integer.parseInt(br.readLine());
+			System.out.println(dp[input]);
+		}
+
+	}
+
+}
