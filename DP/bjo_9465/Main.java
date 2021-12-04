2be12de8bed077bc5d0a82d590de3813ebd399c7
Kyungwoon Na <nakw0410@gmail.com>
2021-12-04 19:43:55 +0900

    feat: 백준 9465번 (스티커) // stickers배열 제거

--- DP/bjo_9465/Main.java
+++ DP/bjo_9465/Main.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -13,26 +14,24 @@ public class Main {
 		for (int i = 0; i < T; i++) {
 			int n = Integer.parseInt(br.readLine());
 
-			int[][] stickers = new int[2][n + 1];
 			int[][] dp = new int[2][n + 1];
 
 			for (int j = 0; j < 2; j++) {
-				String[] inputs = br.readLine().split(" ");
+				String[] str = br.readLine().split(" ");
 				for (int k = 1; k <= n; k++) {
-					stickers[j][k] = Integer.parseInt(inputs[k - 1]);
+					dp[j][k] = Integer.parseInt(str[k - 1]);
 				}
 			}
 
-			// 첫번째 열은 자기 자신이 최대이므로 자기 자신의 값으로 초기화
-			dp[0][1] = stickers[0][1];
-			dp[1][1] = stickers[1][1];
-
-			for (int j = 2; j <= n; j++) {
-				dp[0][j] = Math.max(dp[1][j - 1], dp[1][j - 2]) + stickers[0][j];
-				dp[1][j] = Math.max(dp[0][j - 1], dp[0][j - 2]) + stickers[1][j];
+			for (int k = 2; k <= n; k++) {
+				dp[0][k] = Math.max(dp[1][k - 1], dp[1][k - 2]) + dp[0][k];
+				dp[1][k] = Math.max(dp[0][k - 1], dp[0][k - 2]) + dp[1][k];
 			}
 
 			System.out.println(Math.max(dp[0][n], dp[1][n]));
+
 		}
+
 	}
+
 }
