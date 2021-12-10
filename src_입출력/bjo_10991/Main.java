cac3cea75f21201b6a703fe9bbab021c721f525f
Kyungwoon Na <nakw0410@gmail.com>
2021-12-10 13:45:38 +0900

    feat: 백준 10991번 (별 찍기 -16)

--- src_입출력/bjo_10991/Main.java
+++ src_입출력/bjo_10991/Main.java
@@ -1,5 +1,19 @@
 package bjo_10991;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+			int N = sc.nextInt();
+			
+			for(int i = 0; i < N; i++) {
+				for(int j = 1; j < N-i; j++) {
+					System.out.print(" ");
+				}for(int k = 0; k < i+1; k++) {
+					System.out.print("* ");
+				}
+				System.out.println();
+			}
+	}
 }
