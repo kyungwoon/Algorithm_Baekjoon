bb6a3dfe6437c36ec494acd3f10af21d69e2521e
kyungwoon <nakw0410@gmail.com>
2022-03-02 19:52:15 +0900

    feat : 백준_1110_더하기사이클

--- bjo_study/chapter_1_기본수학/bjo_1110_더하기사이클/Main.java
+++ bjo_study/chapter_1_기본수학/bjo_1110_더하기사이클/Main.java
@@ -4,6 +4,12 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+/*
+    1. 주어진 값을 십의 자리, 일의 자리를 각각 구하고 더해준다.
+    2. temp값을 (1번에서 구한 일의 자리) +(합의 일의 자리)로 초기화 해준다.
+    3. 반복문으로 1,2번을 반복하면서  temp값과 최초 주어진 N값과 동일할 때까지 반복
+ */
+
 public class Main {
     public static void main(String[] args) throws NumberFormatException, IOException {
 
