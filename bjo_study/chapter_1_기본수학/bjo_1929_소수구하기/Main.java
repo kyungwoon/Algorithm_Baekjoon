3534bae5bc7f6bafdda0eb5cee5a33bf1f4353d9
kyungwoon <nakw0410@gmail.com>
2022-03-06 23:42:22 +0900

    feat : 백준_1929_소수구하기

--- bjo_study/chapter_1_기본수학/bjo_1929_소수구하기/Main.java
+++ bjo_study/chapter_1_기본수학/bjo_1929_소수구하기/Main.java
@@ -3,11 +3,30 @@ package chapter_1_기본수학.bjo_1929_소수구하기;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        int M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+
+        boolean[] prime = new boolean[N + 1];
+
+        for(int i = 2; i <= N; i++) {
+            if(prime[i]) continue;
+
+            if(i >= M) sb.append(i).append('\n');
+
+            for(int j = i + i; j <= N; j += i) {
+                prime[j] = true;
+            }
+        }
+
+        System.out.println(sb);
     }
+
 }
