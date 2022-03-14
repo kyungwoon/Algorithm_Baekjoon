f2bdd8f9215879700e5a1da0867cfc384905dcd9
kyungwoon <nakw0410@gmail.com>
2022-03-14 21:51:32 +0900

    feat : 백준_11050_이항계수

---
+++ bjo_study/chapter_2_정수론_조합론_그리디_브루트포/bjo_11050_이항계수/Main.java
@@ -0,0 +1,28 @@
+package chapter_2_정수론_조합론_그리디_브루트포.bjo_11050_이항계수;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+
+        System.out.println(factorial(N) / (factorial(N - K) * factorial(K)));
+    }
+
+    public static int factorial(int n) {
+        int result = 1;
+        for (int i = n; i > 0; i--) {
+            result *= i;
+        }
+        return result;
+    }
+}
