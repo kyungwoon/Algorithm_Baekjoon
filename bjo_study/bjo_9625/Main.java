ac0d1d20e51aa523b4d56438cc2f5d23935e2402
kyungwoon <nakw0410@gmail.com>
2022-08-03 19:30:42 +0900

    feat : 백준_9625_BABBA

---
+++ bjo_study/bjo_9625/Main.java
@@ -0,0 +1,25 @@
+package bjo_9625;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int K = Integer.parseInt(br.readLine());
+        int[] aArr = new int[45];    // A의 개수
+        int[] bArr = new int[45];    // B의 개수
+        aArr[0] = 0;
+        aArr[1] = 1;
+        bArr[0] = 1;
+        bArr[1] = 1;
+
+        for (int i = 2; i < 45; i++) {
+            aArr[i] = aArr[i - 2] + aArr[i - 1];
+            bArr[i] = bArr[i - 2] + bArr[i - 1];
+        }
+        System.out.println(aArr[K - 1] + " " + bArr[K - 1]);
+    }
+}
