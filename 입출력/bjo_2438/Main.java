b5ef7aaf523361d3463c5a68dc60e87fc8fe4799
kyungwoon <nakw0410@gmail.com>
2022-09-20 16:13:10 +0900

    백준_2438_별 찍기 -1

--- 입출력/bjo_2438/Main.java
+++ 입출력/bjo_2438/Main.java
@@ -8,6 +8,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+
         int N = Integer.parseInt(br.readLine());
 
         for (int i = 0; i < N; i++) {
